6a2bf809f40f143cd1c68833df9b7b553aca3e17
cosminchiriac97 <chiriac.cosmin97@gmail.com>
2017-06-27 20:49:46 +0300

    [Added] API

---
+++ KingOfPetsAPI/KingOfPetsAPI/Controllers/ValuesController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using KingOfPetsAPI.Model;
+using System;
+using KingOfPetsAPI.Util;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace KingOfPetsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+      
+        // GET api/values?id=lastId&limit=LIMIT
+        [HttpGet]
+        public IActionResult EmailPagination([FromQuery]string id, [FromQuery]string limit)
+        {
+            if(limit == null)
+            {
+                return BadRequest();
+            }
+            if (id == null)
+            {
+                using (var db = new EmailModel()) {
+                    var limitInt = Int32.Parse(limit);
+                    var emails = db.Emails.FromSql("SELECT *  FROM Emails ORDER BY ID ASC LIMIT {0}",limitInt).ToList();
+                    return Ok(emails);
+                }
+            }
+            else
+            {
+                using (var db = new EmailModel())
+                {
+                    var email = db.Emails.Find(Int32.Parse(id));
+                    if (email == null)
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        var limitInt = Int32.Parse(limit);
+                        var idInt = Int32.Parse(id);
+                        var emails = db.Emails.FromSql("SELECT *  FROM Emails WHERE ID>{0} ORDER BY ID ASC LIMIT {1}", idInt,limitInt).ToList();
+                        return Ok(emails);
+                    }
+                }
+            }
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]Email emailJson)
+        {
+            if (emailJson.email.Equals(null)) { return BadRequest(emailJson); }
+
+            if (!EmailSender.sendEmail("chiriac.cosmin97@gmail.com", emailJson.email)) { return BadRequest(emailJson); }
+
+            using (var db = new EmailModel())
+            {
+                db.Emails.Add(new Email { email = emailJson.email });    
+                var count = db.SaveChanges();
+                return Ok(emailJson);
+            }
+        }
+
+    }
+}
---
+++ KingOfPetsAPI/KingOfPetsAPI/Util/EmailSender.cs
@@ -0,0 +1,60 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KingOfPetsAPI.Util
+{
+    public class EmailSender
+    {
+        public static Boolean sendEmail(string fromAddress, string toAddress)
+        {
+            try
+            {
+
+                //From Address
+                string FromAddress = fromAddress;
+                string FromAdressTitle = "TEST";
+
+                //To Address
+                string ToAddress = toAddress;
+                string ToAdressTitle = "TEST";
+                string Subject = "Hello Andone!";
+                string BodyContent = "Hello Andone, this is an email sended from ASP.NET Core application! :) nice";
+
+                //Smtp Server
+                string SmtpServer = "smtp.gmail.com";
+
+                //Smtp Port Number
+                int SmtpPortNumber = 587;
+
+                var mimeMessage = new MimeMessage();
+                mimeMessage.From.Add(new MailboxAddress(FromAdressTitle, FromAddress));
+                mimeMessage.To.Add(new MailboxAddress(ToAdressTitle, ToAddress));
+                mimeMessage.Subject = Subject;
+                mimeMessage.Body = new TextPart("plain")
+                {
+                    Text = BodyContent
+
+                };
+
+                using (var client = new SmtpClient())
+                {
+
+                    client.Connect(SmtpServer, SmtpPortNumber, false);
+                    client.Authenticate("chiriac.cosmin97@gmail.com", "021897081401f25df189/");
+                    client.Send(mimeMessage);
+                    client.Disconnect(true);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
